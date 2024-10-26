7e9227014fc85ac32db159a091ccd86345bf7228
mario-sica/Mario <mario97sica@gmail.com>
2024-10-26 07:33:27 +0200

    Updated: Conditional Rendering - 1 to CR - 2

--- Conditional rendering/Age.jsx
+++ Conditional rendering/Age.jsx
@@ -1,7 +1,7 @@
 export function Age({ age }) {
   return (
     <div>
-      <p>Your age is {age}.</p>
+      {age > 18 ? <p>Your age is {age}.</p> : <p>You are very young!</p>}
     </div>
   );
 }
